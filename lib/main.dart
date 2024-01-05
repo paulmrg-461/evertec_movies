ccfa7e0b98aa7e2255b0fd850dd798880792657e
Paul Realpe <paul.realpe@ia-aplicada.com>
2024-01-04 19:10:27 -0500

    Feat: Config: ApiKey added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
-import 'package:evertec_movies/config/router/app_router.dart';
-import 'package:evertec_movies/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'package:evertec_movies/config/router/app_router.dart';
+import 'package:evertec_movies/config/theme/app_theme.dart';
+
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
