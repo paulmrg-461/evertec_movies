b36d2b16cbb395b6dce16b7521329941aad58438
Paul Realpe <paul.realpe@ia-aplicada.com>
2024-01-04 20:35:26 -0500

    Feat: Infrastructure: Movies: Implementation of MoviesRepository

--- lib/infrastructure/datasources/movie_db_datasource.dart
+++ lib/infrastructure/datasources/movie_db_datasource.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:evertec_movies/domain/datasources/movies_datasource.dart';
 import 'package:evertec_movies/domain/entities/movie_entity.dart';
 import 'package:evertec_movies/config/constants/environment.dart';
+import 'package:evertec_movies/infrastructure/mappers/movie_mapper.dart';
+import 'package:evertec_movies/infrastructure/models/the_movie_db/movie_db_response.dart';
 
 class MovieDbDatasource implements MoviesDatasource {
   final Dio dio = Dio(BaseOptions(
@@ -14,7 +16,12 @@ class MovieDbDatasource implements MoviesDatasource {
   @override
   Future<List<MovieEntity>> getUpcomingMovies({int page = 1}) async {
     final Response response = await dio.get('/movie/upcoming');
-    final List<MovieEntity> movies = [];
+
+    final MovieDbResponse movieDbResponse =
+        MovieDbResponse.fromJson(response.data);
+    final List<MovieEntity> movies = movieDbResponse.results
+        .map((e) => MovieMapper.movieDbToEntity(e))
+        .toList();
     return movies;
   }
 }
---
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,12 @@
+import 'package:evertec_movies/domain/datasources/movies_datasource.dart';
+import 'package:evertec_movies/domain/entities/movie_entity.dart';
+import 'package:evertec_movies/domain/repositories/movies_repository.dart';
+
+class MovieRepositoryImpl implements MoviesRepository {
+  final MoviesDatasource moviesDatasource;
+
+  MovieRepositoryImpl({required this.moviesDatasource});
+  @override
+  Future<List<MovieEntity>> getUpcomingMovies({int page = 1}) =>
+      moviesDatasource.getUpcomingMovies(page: page);
+}
