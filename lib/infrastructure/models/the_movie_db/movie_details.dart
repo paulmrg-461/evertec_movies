c17403fb89b291de08ced2581be326a829352539
Paul Realpe <paul.realpe@ia-aplicada.com>
2024-01-06 00:54:11 -0500

    Feat: IconApp: Updated icon app

--- lib/infrastructure/models/the_movie_db/movie_details.dart
+++ lib/infrastructure/models/the_movie_db/movie_details.dart
@@ -139,7 +139,7 @@ class BelongsToCollection {
         id: json["id"],
         name: json["name"],
         posterPath: json["poster_path"],
-        backdropPath: json["backdrop_path"],
+        backdropPath: json["backdrop_path"] ?? '',
       );
 
   Map<String, dynamic> toJson() => {
