ea626fca810a06385c28fe7b5908ebf74aa872ed
Paul Realpe <paul.realpe@ia-aplicada.com>
2024-01-05 23:59:45 -0500

    Feat: Presentation: Delegates: SearchMovies delegate created

--- lib/config/helpers/human_formats.dart
+++ lib/config/helpers/human_formats.dart
@@ -1,7 +1,8 @@
 import 'package:intl/intl.dart';
 
 class HumanFormat {
-  static String number(double number) =>
-      NumberFormat.compactCurrency(decimalDigits: 0, symbol: '', locale: 'en')
+  static String number(double number, [int decimals = 0]) =>
+      NumberFormat.compactCurrency(
+              decimalDigits: decimals, symbol: '', locale: 'en')
           .format(number);
 }
